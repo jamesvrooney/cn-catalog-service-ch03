099f859d0309f3f457b84c4a57ec270f78586700
jamesvrooney <jamesvrooney@hotmail.com>
2024-04-01 07:16:48 +0100

    Dummy commit 3.

--- src/main/java/com/jamesvrooney/controller/BookController.java
+++ src/main/java/com/jamesvrooney/controller/BookController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("books")
 public class BookController {
 
-    // Just
+    // Just a cover
     private final BookService bookService;
 
     public BookController(BookService bookService) {
